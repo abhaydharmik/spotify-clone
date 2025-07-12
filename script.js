cd81cab4f68ddfa4e0de374034c94d6f2c33bbf8
Abhay Dharmik <dharmikabhay362@gmail.com>
2025-07-13 04:25:43 +0530

    some modification

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log("Let's Write JavaScript");
+let currentSong = new Audio();
 
 async function getSongs() {
   let a = await fetch("http://127.0.0.1:4000/songs/");
@@ -14,25 +15,57 @@ async function getSongs() {
   for (let index = 0; index < as.length; index++) {
     const element = as[index];
     if (element.href.endsWith(".mp3")) {
-      songs.push(element.href);
+      songs.push(element.href.split("/songs/")[1]);
     }
   }
   return songs;
 }
 
+const playMusic = (track) => {
+  // let audio = new Audio("/songs/" + track)
+  currentSong.src = "/songs/" + track 
+  play.src = "pause.svg"
+  currentSong.play()
+}
+
 async function main() {
+
   let songs = await getSongs();
   console.log(songs);
 
-  // wait for user interaction before playing
-  window.addEventListener("click", function playOnce() {
-    var audio = new Audio(songs[0]);
-    audio.play().catch((err) => {
-      console.error("Playback failed:", err);
-    });
+  // Show all the songs in the playlist
+  let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+  for (const song of songs) {
+   songUL.innerHTML = songUL.innerHTML + `<li><img class="invert" src="music.svg" alt="">
+                                            <div class="info">
+                                              <div>${song.replaceAll("%20", " ")}</div>
+                                              <div>Song Artist</div>
+                                            </div>
+                                            <div class="playnow">
+                                              <span>Play Now</span>
+                                              <img class="invert" src="play.svg" alt="">
+                                            </div>
+                                          </li>` 
+  }
 
-    window.removeEventListener("click", playOnce); // remove listener after first play
-  });
+  // Attach an event listener to each song
+  Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e =>{
+    e.addEventListener("click", element => {
+      console.log(e.querySelector(".info").firstElementChild.innerHTML)
+      playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+    })
+  })
+
+  play.addEventListener("click", () => {
+    if(currentSong.paused){
+      currentSong.play()
+      play.src = "pause.svg"
+    }else {
+      currentSong.pause()
+      play.src = "play.svg"
+    }
+  })
+
 }
 
 main();
